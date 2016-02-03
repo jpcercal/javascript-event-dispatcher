9844efbcc860b623c956f6a98f8df00161afe1c9
João Paulo Cercal <jpcercal@gmail.com>
2016-02-03 07:12:49 -0300

    Fixed the problem with scope of variables and changed the project name again.

--- src/event-dispatcher.js
+++ src/event-dispatcher.js
@@ -1,23 +1,33 @@
-(function() {
+/**
+ * EventDispatcher
+ *
+ * @param Element|null element
+ */
+var EventDispatcher = function (element) {
+    var el = element;
 
-    var EventDispatcher = function (element) {
-        var el = element;
+    return {
 
-        return {
-            'dispatch': function (eventName) {
-                if (document.createEvent) {
-                    var evt = document.createEvent('UIEvents');
-                    evt.initEvent(eventName, true, true);
-                    el.dispatchEvent(evt);
-                } else if (document.createEventObject) {
-                    el.fireEvent('on' + eventName);
-                } else if (typeof el['on' + eventName] == 'function') {
-                    el['on' + eventName]();
-                }
-
-                return this;
+        /**
+         * Dispatch an event.
+         *
+         * @param  string eventName the event name that will be dispatched.
+         * @param  string eventType the event type the default value is "UIEvents".
+         *
+         * @return EventDispatcher
+         */
+        'dispatch': function (eventName, eventType) {
+            if (document.createEvent) {
+                var evt = document.createEvent(typeof eventType === 'undefined' ? 'UIEvents' : eventType);
+                evt.initEvent(eventName, true, true);
+                el.dispatchEvent(evt);
+            } else if (document.createEventObject) {
+                el.fireEvent('on' + eventName);
+            } else if (typeof el['on' + eventName] == 'function') {
+                el['on' + eventName]();
             }
-        };
-    };
 
-})();
+            return this;
+        }
+    };
+};
